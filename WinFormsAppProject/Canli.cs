de6f6f73b30a61132ecc838cc9467ea03f99728e
gkrctn <gkr.cetin@gmail.com>
2022-01-03 20:48:31 +0300

    Add project files.

---
+++ WinFormsAppProject/Canli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsAppProject
+{
+    public class Canli
+    {
+        public int Yas { get; set; }
+
+        void OksijenAl()
+        {
+
+        }
+       
+
+    }
+}
---
+++ WinFormsAppProject/insan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsAppProject
+{
+    public class insan : Canli
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
